edbf7c312a24abe950c50ec26f4cde2ab6b934ac
Aura Zulaikha <auraulaikha00@gmail.com>
2023-03-15 23:11:40 +0700

    latihan3 modul 6(for loop) tgl 15\03\23

---
+++ src/main/java/aura150323/latihan3_forloop.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aura150323;
+import java.io.*;
+// LATIHAN 3 MODUL 6 ( forloop )
+/**
+ *
+ * @author ACER
+ */
+public class latihan3_forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) { 
+            System.out.println("aura");
+        }
+    }
+}
