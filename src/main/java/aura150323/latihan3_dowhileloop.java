2f429532e5ab9a2c1a277163bfd2ffdaaa5a2eb4
Aura Zulaikha <auraulaikha00@gmail.com>
2023-03-15 23:09:06 +0700

    latihan3 modul6 (do while loop ) tgl 15\03\23

---
+++ src/main/java/aura150323/latihan3_dowhileloop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aura150323;
+import java.io.*;
+// LATIHAN 3 MODUL 6 ( dowhileloop )
+/**
+ *
+ * @author ACER
+ */
+public class latihan3_dowhileloop {
+    public static void main(String[] args) {
+        int banyak = 0;
+        do {
+            System.out.println("aura");
+            banyak++;
+        } while (banyak < 100);
+    }
+}
