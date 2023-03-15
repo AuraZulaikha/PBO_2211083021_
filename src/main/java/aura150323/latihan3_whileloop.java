8110a14d1f198711a3050033d6b20ab7fb9e64b0
Aura Zulaikha <auraulaikha00@gmail.com>
2023-03-15 23:06:20 +0700

    latihan3 modul 6 whileloop tgl 15\03\23

---
+++ src/main/java/aura150323/latihan3_whileloop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aura150323;
+import java.io.*;
+// LATIHAN 3 MODUL 6 ( whileloop )
+/**
+ *
+ * @author ACER
+ */
+public class latihan3_whileloop {
+    public static void main (String [] args ){
+        int banyak = 0;
+        while (banyak < 100) {
+            System.out.println("Aura ");
+            banyak++;
+        }
+    }
+}
