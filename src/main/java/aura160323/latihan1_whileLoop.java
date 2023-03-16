fcb493fbfb0b68ceea4bb73ad39616c976184daa
Aura Zulaikha <auraulaikha00@gmail.com>
2023-03-16 11:14:25 +0700

    latihan1 modul 7 (whileloop) tfl 16\03\23

---
+++ src/main/java/aura160323/latihan1_whileLoop.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aura160323;
+import java.util.Scanner;
+// LATIHAN 1 MODUL 7 MENGGUNAKAN WHILE LOOP
+/**
+ *
+ * @author ACER
+ */
+public class latihan1_whileLoop {
+    public static void main (String []args ){
+        String days [] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        int i =0 ;
+        
+        Scanner in = new Scanner(System.in);
+        
+        while (i< days.length ){
+            System.out.println("\t"+days[i]);
+            i++;
+        }      
+    }
+}
