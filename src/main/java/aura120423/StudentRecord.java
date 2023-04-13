9907c86ac068ed6cb19f5e3192c5e647c62710db
Aura Zulaikha <auraulaikha00@gmail.com>
2023-04-13 10:40:44 +0700

    Modul 11

---
+++ src/main/java/aura120423/StudentRecord.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aura120423;
+
+/**
+ *
+ * @author ACER
+ */
+public class StudentRecord {
+    int nilai;
+    protected String name; 
+    protected String address; 
+    protected int age; 
+    protected double mathGrade; 
+    protected double englishGrade; 
+    protected double scienceGrade; 
+    protected double average; 
+    
+    public StudentRecord(){  
+        studentCount++;
+    }
+    
+    public StudentRecord(String temp){ 
+        this.name = temp; 
+        studentCount++;
+    } 
+    
+    public StudentRecord(String name, String address){ 
+        this.name = name; 
+        this.address = address; 
+        studentCount++;
+    } 
+    
+    public StudentRecord(double mGrade, double eGrade, double sGrade){ 
+        mathGrade = mGrade; 
+        englishGrade = eGrade; 
+        scienceGrade = sGrade; 
+        studentCount++;
+    }
+    
+    //area penulisan kode selanjutnya 
+    
+    private static int studentCount;
+    
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public int getAge(){
+        return age; 
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public double getmathGrade(){
+        return mathGrade; 
+    }
+    public void setmathGrade(double mathGrade){
+        this.mathGrade = mathGrade;
+    }
+    public double getenglishGrade(){
+        return englishGrade; 
+    }
+    public void setenglishGrade(double englishGrade){
+        this.englishGrade = englishGrade;
+    }
+    public double getscienceGrade(){
+        return scienceGrade; 
+    }
+    public void setscienceGrade(double scienceGrade){
+        this.scienceGrade = scienceGrade;
+    }
+    public double getaverage(){
+        average = (mathGrade + englishGrade + scienceGrade) / 3;
+        return average;
+    }
+    public static int  getstudentCount(){
+        return studentCount;
+        
+    }
+    public void print( String temp ){ 
+        System.out.println("Name              : " + name); 
+        System.out.println("Address           : " + address); 
+        System.out.println("Age               : " + age); 
+    } 
+    public void print(double eGrade, double mGrade, double sGrade) {
+        System.out.println("Name              : " + name); 
+        System.out.println("Math Grade        : " + mGrade); 
+        System.out.println("English Grade     : " + eGrade); 
+        System.out.println("Science Grade     : " + sGrade); 
+    }  
+}
