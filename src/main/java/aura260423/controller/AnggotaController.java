8f221dbae546b97a44924061d509bc30d3a3b66a
Aura Zulaikha <auraulaikha00@gmail.com>
2023-05-03 08:04:53 +0700

    .

---
+++ src/main/java/aura260423/controller/AnggotaController.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aura260423.controller;
+import aura260423.view.*;
+import aura260423.model.*;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+/**
+ *
+ * @author ACER
+ */
+public class AnggotaController {
+    private FormAnggota formAnggota;
+    private Anggota anggota;
+    private AnggotaDao anggotaDao;
+    
+    public AnggotaController(FormAnggota formAnggota){
+        this.formAnggota = formAnggota ;
+        anggotaDao = new AnggotaDaoImpl();
+    }
+    
+    public void bersihForm(){
+        formAnggota.getTxtNobp().setText("");
+        formAnggota.getTxtNama().setText("");
+        formAnggota.getTxtAlamat().setText("");
+    }
+    
+    public void saveAnggota(){
+        anggota = new Anggota();
+        anggota.setNobp(formAnggota.getTxtNobp().getText());
+        anggota.setNama(formAnggota.getTxtNama().getText());
+        anggota.setAlamat(formAnggota.getTxtAlamat().getText());
+        anggotaDao.save(anggota);
+        JOptionPane.showMessageDialog(formAnggota, "Insert ok ");
+    }
+    
+    public void tampil(){
+        DefaultTableModel tabelModel =
+                (DefaultTableModel) formAnggota.getTblAnggota().getModel();
+        tabelModel.setRowCount(0);
+        List<Anggota> list = anggotaDao.getAllAnggota();
+        for (Anggota anggota1 : list){
+            Object row[]= {
+                anggota1.getNobp(),
+                anggota1.getNama(),
+                anggota1.getAlamat(),
+            };
+            tabelModel.addRow(row);
+        }
+    }
+}
---
+++ src/main/java/aura270423/model/BukuDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package aura270423.model;
+import java.util.*;
+
+/**
+ *
+ * @author ACER
+ */
+public interface BukuDao {
+    void save(Buku buku);
+    void update(int index, Buku buku);
+    void delete (int index);
+    Buku getBuku(int index);
+    public List<Buku> getAllBuku();
+}
